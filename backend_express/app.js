2b15fba0b2aed642b4b9e697fcd46f3a7a017116
Masahiro.Takeyama <takeyama.masahiro.alh@gmail.com>
2021-06-15 13:47:42 +0900

    react_express　コピー

---
+++ backend_express/app.js
@@ -0,0 +1,21 @@
+'use strict';
+
+const express = require('express');
+const app = express();
+
+app.use(express.static('./build'));
+
+app.get('/get/json', (req,res) => {
+  res.json({
+      message:"Hello,world"
+  });
+}); 
+
+if (module === require.main) {
+  const server = app.listen(process.env.PORT || 8080, () => {
+    const port = server.address().port;
+    console.log(`App listening on port ${port}`);
+  });
+}
+
+module.exports = app;
